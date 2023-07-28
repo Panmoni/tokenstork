06fc67e293cfa974a5ddf0396c697785a3498bd8
George Donnelly <me@georgedonnelly.com>
2023-07-28 12:13:07 -0500

    add github api fetch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+console.log("hello world")
+
+// Fetch a user from the GitHub API
+fetch('https://api.github.com/users/georgedonnelly')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
