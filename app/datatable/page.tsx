323954e1b1c4470739dfba297c72dea361083243
George Donnelly <me@georgedonnelly.com>
2023-11-04 17:11:09 -0500

    in progress

--- app/datatable/page.tsx
+++ app/datatable/page.tsx
@@ -25,16 +25,16 @@ import TinyLoader from "@/app/components/TinyLoader";
 // tokenTotalReservedAmount
 
 type tokenData = {
-  icon: string,
-  name: string,
-  description: string,
-  symbol: string,
-  price:  number,
-  circulatingSupply: number, 
-  maxSupply: number,
-  marketCap: number,
-  tvl: number,
-  category: string,
+  icon: string;
+  name: string;
+  description: string;
+  symbol: string;
+  price: number;
+  circulatingSupply: number;
+  maxSupply: number;
+  marketCap: number;
+  tvl: number;
+  category: string;
 };
 
 // TODO: implement comprehensive error handling
@@ -60,25 +60,26 @@ export default function DataTable() {
   // TODO: should this be a more full-fledged function and its own file?
 
   const fetchData = async () => {
-
     // variables
-    const tokenCategory = "d9ab24ed15a7846cc3d9e004aa5cb976860f13dac1ead05784ee4f4622af96ea";
+    const { bchPrice } = useBCHPrice();
+    const tokenCategory =
+      "d9ab24ed15a7846cc3d9e004aa5cb976860f13dac1ead05784ee4f4622af96ea";
     const bcmrServer = "https://bcmr.paytaca.com/api/tokens/";
     const cauldronServer = "https://cauldronapi.panmoni.com";
     const chaingraphServer = "https://gql.chaingraph.pat.mn/v1/graphql";
 
-    const { bchPrice } = useBCHPrice();
-
     try {
-      const bcmrAPI = await fetch(
-        bcmrServer + tokenCategory
-      );
+      const bcmrAPI = await fetch(bcmrServer + tokenCategory);
       const bcmrData = await bcmrAPI.json();
 
       const tokenDecimals = bcmrData.token.decimals;
 
       const cauldronPriceAPI = await fetch(
-        cauldronServer + "/token_price?category=" + tokenCategory + "&decimals=" + tokenDecimals
+        cauldronServer +
+          "/token_price?category=" +
+          tokenCategory +
+          "&decimals=" +
+          tokenDecimals
       );
       const cauldronPriceData = await cauldronPriceAPI.json();
 
@@ -88,77 +89,87 @@ export default function DataTable() {
       const cauldronLiquidityData = await cauldronLiquidityAPI.json();
 
       // Get the max supply from chaingraph
-      const maxSupplyData = await queryTotalSupplyFT(tokenCategory, chaingraphServer);
+      const maxSupplyData = await queryTotalSupplyFT(
+        tokenCategory,
+        chaingraphServer
+      );
 
-        if (
-          !maxSupplyData.data ||
-          !maxSupplyData.data.transaction ||
-          !maxSupplyData.data.transaction[0] ||
-          !maxSupplyData.data.transaction[0].outputs
-        ) {
-          throw new Error("Invalid response structure");
-        }
+      if (
+        !maxSupplyData.data ||
+        !maxSupplyData.data.transaction ||
+        !maxSupplyData.data.transaction[0] ||
+        !maxSupplyData.data.transaction[0].outputs
+      ) {
+        throw new Error("Invalid response structure");
+      }
 
-        // TODO: what if there simply is no decimals value?
+      // TODO: what if there simply is no decimals value?
 
+      if (isNaN(tokenDecimals) || tokenDecimals < 0 || tokenDecimals > 100) {
+        throw new Error("Invalid decimals value");
+      }
+
+      let tokenTotalSupply = maxSupplyData.data.transaction[0].outputs.reduce(
+        (total: bigint, output: { fungible_token_amount: string }) => {
+          if (typeof output.fungible_token_amount !== "string") {
+            throw new Error("Invalid token amount");
+          }
+          let amount = BigInt(output.fungible_token_amount);
+          return total + amount;
+        },
+        BigInt(0)
+      );
+
+      // Convert to a decimal form
+      tokenTotalSupply = tokenTotalSupply / BigInt(Math.pow(10, tokenDecimals));
+
+      // convert it back to a number
+      tokenTotalSupply = tokenTotalSupply === "" ? "0" : tokenTotalSupply;
+      tokenTotalSupply = Number(tokenTotalSupply);
+
+      const tokenReservedSupplyData = await queryAuthchainLength(
+        tokenCategory,
+        chaingraphServer
+      );
+
+      if (
+        !tokenReservedSupplyData.data.transaction[0].authchains[0].authhead
+          .identity_output[0].fungible_token_amount
+      ) {
+        let tokenTotalReservedAmount = 0;
+        return tokenTotalReservedAmount;
+      } else {
         if (isNaN(tokenDecimals) || tokenDecimals < 0 || tokenDecimals > 100) {
           throw new Error("Invalid decimals value");
         }
 
-        let tokenTotalSupply = maxSupplyData.data.transaction[0].outputs.reduce(
-          (total: bigint, output: { fungible_token_amount: string }) => {
-            if (typeof output.fungible_token_amount !== "string") {
-              throw new Error("Invalid token amount");
-            }
-            let amount = BigInt(output.fungible_token_amount);
-            return total + amount;
-          },
-          BigInt(0)
-        );
+        let tokenTotalReservedAmount =
+          tokenReservedSupplyData.data.transaction?.[0]?.authchains?.[0]
+            ?.authhead?.identity_output?.[0]?.fungible_token_amount;
+
+        tokenTotalReservedAmount = BigInt(tokenTotalReservedAmount);
 
         // Convert to a decimal form
-        tokenTotalSupply = tokenTotalSupply / BigInt(Math.pow(10, tokenDecimals));
+        tokenTotalReservedAmount =
+          tokenTotalReservedAmount / BigInt(Math.pow(10, tokenDecimals));
 
         // convert it back to a number
-        tokenTotalSupply = tokenTotalSupply === "" ? "0" : tokenTotalSupply;
-        tokenTotalSupply = Number(tokenTotalSupply);
-
-      const tokenReservedSupplyData = await queryAuthchainLength(tokenCategory, chaingraphServer);
-
-        if (
-          !tokenReservedSupplyData.data.transaction[0].authchains[0].authhead
-            .identity_output[0].fungible_token_amount
-        ) {
-          let tokenTotalReservedAmount = 0;
-          return tokenTotalReservedAmount;
-        } else {
-          if (isNaN(tokenDecimals) || tokenDecimals < 0 || tokenDecimals > 100) {
-            throw new Error("Invalid decimals value");
-          }
-
-          let tokenTotalReservedAmount =
-          tokenReservedSupplyData.data.transaction?.[0]?.authchains?.[0]?.authhead
-              ?.identity_output?.[0]?.fungible_token_amount;
-
-          tokenTotalReservedAmount = BigInt(tokenTotalReservedAmount);
-
-          // Convert to a decimal form
-          tokenTotalReservedAmount =
-          tokenTotalReservedAmount / BigInt(Math.pow(10, decimals));
-
-          // convert it back to a number
-          tokenTotalReservedAmount =
+        tokenTotalReservedAmount =
           tokenTotalReservedAmount === "" ? "0" : tokenTotalReservedAmount;
-          tokenTotalReservedAmount = Number(tokenTotalReservedAmount);
-        }
+        tokenTotalReservedAmount = Number(tokenTotalReservedAmount);
+        return tokenTotalReservedAmount;
+      }
 
-      let tokenUSDPrice = ((cauldronPriceData.result.buy +cauldronPriceData.result.sell)/2) * bchPrice.toFixed(2);
+      let tokenUSDPrice =
+        ((cauldronPriceData.result.buy + cauldronPriceData.result.sell) / 2) *
+        bchPrice.toFixed(2);
 
       let tokenCirculatingSupply = tokenTotalSupply - tokenTotalReservedAmount;
 
       let tokenMarketCap = tokenCirculatingSupply * tokenUSDPrice;
 
-      let totalValueLocked = satoshisToBCH(cauldronLiquidityData.result.bch) * bchPrice.toFixed(2);
+      let totalValueLocked =
+        satoshisToBCH(cauldronLiquidityData.result.bch) * bchPrice.toFixed(2);
 
       // Combine data from all APIs into a single object for ease of use
       const tokenData = {
@@ -166,8 +177,8 @@ export default function DataTable() {
         name: bcmrData.name,
         description: bcmrData.token.description,
         symbol: bcmrData.token.symbol,
-        price:  tokenUSDPrice,
-        circulatingSupply: tokenCirculatingSupply, 
+        price: tokenUSDPrice,
+        circulatingSupply: tokenCirculatingSupply,
         maxSupply: tokenTotalSupply,
         marketCap: tokenMarketCap,
         tvl: totalValueLocked,
@@ -196,7 +207,7 @@ export default function DataTable() {
   // use tokenIds recursively
 
   // itemRow still has a lot of important stuff
-  
+  // /table has formatting for Tremor
 
   return (
     <table>
@@ -216,13 +227,15 @@ export default function DataTable() {
           </tr>
         ) : (
           <tr>
-            <td colSpan={7}><TinyLoader /></td>
+            <td colSpan={7}>
+              <TinyLoader />
+            </td>
           </tr>
         )}
       </tbody>
       {toastMessage && <Toast message={toastMessage} />}
     </table>
   );
-};
+}
 
 // TODO: use humanizeBigNumber in the presentation stage
--- todo.md
+++ todo.md
@@ -1,8 +1,8 @@
 // TODO: rationalize types, understand them
 // TODO: implement TinyLoader
 // TODO: replace fontawesome icons with svgs
+// TODO: place this stuff in utils and just call it from page?
 
 ## typescript
 
 type TokenData and interfaces.ts ?? do i need both and what does it really mean
-
