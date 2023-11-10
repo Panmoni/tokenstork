3682c139c054c90f8e55105bcfad2ea8170df452
George Donnelly <me@georgedonnelly.com>
2023-11-10 10:16:54 -0500

    increase navbar font size and weight

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -50,7 +50,7 @@ export default function Navbar({}: {}) {
                         pathname === item.href
                           ? "border-slate-500 text-gray-900"
                           : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                        "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium no-underline"
+                        "inline-flex items-center px-1 pt-1 border-b-2 text-lg font-semibold no-underline"
                       )}
                       aria-current={pathname === item.href ? "page" : undefined}
                     >
