70625d4ece410f8d9c07c66142e3181079594e49
George Donnelly <me@georgedonnelly.com>
2024-01-06 12:50:50 -0500

    add CORS wildcard

--- pages/api/blockchainData.js
+++ pages/api/blockchainData.js
@@ -14,7 +14,7 @@ await electrum.ready();
 console.log("Connected to Electrum servers.");
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
