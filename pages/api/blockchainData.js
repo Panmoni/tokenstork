c098faf9621d31a1a82f3feb1b1d12ec76c043df
George Donnelly <me@georgedonnelly.com>
2024-01-06 12:46:52 -0500

    try manual CORS header

--- pages/api/blockchainData.js
+++ pages/api/blockchainData.js
@@ -14,6 +14,15 @@ await electrum.ready();
 console.log("Connected to Electrum servers.");
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle OPTIONS request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   try {
     // await NextCors(req, res, {
     //   methods: ["GET"],
